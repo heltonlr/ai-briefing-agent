9af9ec1228dd682e3118e0d856f345054c770558
Helton LR <heltonlr27@gmail.com>
2026-02-24 14:43:08 -0300

    feat: add Vite project structure

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BriefingAgent from './briefing-agent.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BriefingAgent />
+  </React.StrictMode>
+)
